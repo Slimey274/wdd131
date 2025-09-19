837669912e20fa1510538e9839f4d96c261e1743
Slimey274 <briguy030102@gmail.com>
2025-09-19 16:53:16 -0600

    dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -1,15 +1,17 @@
 const themeSelector = document.querySelector('#theme-selector');
-function changeTheme() {
-    const body = document.body;
-    const logo = document.querySelector('#logo'); 
+const logo = document.querySelector('#logo');
+const body = document.body;
+
+function changeTheme() { 
     if (themeSelector.value === 'dark') {
-    body.classList.add('dark');
-    logo.src = 'byui-logo-white.png';   // replace with correct path to white logo
-    } 
-    else {
-    body.classList.remove('dark');
-    logo.src = 'byui-logo_blue.jpg';    // replace with correct path to blue logo
+        body.classList.add('dark');
+        logo.src = 'byui-logo-white.jpg';   // replace with correct path to white logo
+    } else {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.jpg';    // replace with correct path to blue logo
     }
 }
+
+changeTheme();
 
 themeSelector.addEventListener('change', changeTheme);
