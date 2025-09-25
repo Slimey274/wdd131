818b7420fedb0f2d7e5f7b3363cb749eafd58f57
Slimey274 <briguy030102@gmail.com>
2025-09-25 10:37:30 -0600

    finished the debugging activity

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,8 +1,16 @@
 let PI = 3.14;
-let radius = 3;
+// let radius = 3;
 let area = 0;
-area = radius * radius * PI;
+
+function circleArea(radius) {
+    const area = radius * radius * PI;
+    return area;
+}
+// area = radius * radius * PI;
+
+area = circleArea(3);
 console.log('Area 1:', area);
-radius = 4;
-area = radius * radius * PI;
+// radius = 4;
+// area = radius * radius * PI;
+area = circleArea(4);  
 console.log('Area 2:', area);
